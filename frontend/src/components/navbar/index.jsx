52b66c09db13cc438e9c5c2d4e772ac2d8d05dd0
andre0000 <122487767+sovac0@users.noreply.github.com>
2025-05-14 18:24:29 +0100

    again?

--- frontend/src/components/navbar/index.jsx
+++ frontend/src/components/navbar/index.jsx
@@ -1,106 +1,40 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import LoginModal from '../../modals/loginModal';
-import RegisterModal from '../../modals/registerModal';
+import { FaSearch, FaUser, FaShoppingBag } from 'react-icons/fa';
+import './styles.css';
 
 const Navbar = () => {
-  const [showLoginModal, setShowLoginModal] = useState(false);
-  const [showRegisterModal, setShowRegisterModal] = useState(false);
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    const storedUser = sessionStorage.getItem('user');
-    if (storedUser) {
-      setUser(JSON.parse(storedUser));
-    }
-  }, []);
-
-  const handleLoginSuccess = (userData) => {
-    setUser(userData);
-    setShowLoginModal(false);
-    setShowRegisterModal(false);
-  };
-
-  const openRegisterFromLogin = () => {
-    setShowLoginModal(false);
-    setShowRegisterModal(true);
-  };
-
   return (
-    <>
-      <nav className='navbar navbar-expand-lg navbar-light bg-light px-3'>
-        <a className='navbar-brand' href='/'>
+    <nav className='navbar navbar-expand-lg custom-navbar'>
+      <div className='container-fluid d-flex justify-content-between align-items-center'>
+        <a className='navbar-brand minimal-brand' href='#'>
           Glamour
         </a>
-        <button
-          className='navbar-toggler'
-          type='button'
-          data-bs-toggle='collapse'
-          data-bs-target='#navbarTogglerDemo03'
-          aria-controls='navbarTogglerDemo03'
-          aria-expanded='false'
-          aria-label='Toggle navigation'
-        >
-          <span className='navbar-toggler-icon' />
-        </button>
 
-        <div className='collapse navbar-collapse' id='navbarTogglerDemo03'>
-          <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
-            <li className='nav-item'>
-              <a className='nav-link' href='#'>
-                Products
-              </a>
-            </li>
-            <li className='nav-item'>
-              <a
-                className='nav-link disabled'
-                href='#'
-                tabIndex='-1'
-                aria-disabled='true'
-              ></a>
-            </li>
-          </ul>
-
-          <form className='d-flex me-3' role='search'>
+        <div className='d-flex align-items-center gap-3'>
+          <form className='search-form d-flex align-items-center'>
             <input
-              className='form-control me-2'
+              className='form-control form-control-sm minimal-search'
               type='search'
-              placeholder='Search...'
+              placeholder='Search'
               aria-label='Search'
             />
+            <button className='btn btn-sm search-btn' type='submit'>
+              <FaSearch />
+            </button>
           </form>
 
-          <button
-            className='btn btn-outline-primary me-2'
-            onClick={() => !user && setShowLoginModal(true)}
-          >
-            <i className='fas fa-user me-1'></i>
-            {user ? user.name : 'Login'}
+          <button className='btn btn-icon' title='Profile'>
+            <FaUser />
           </button>
 
-          <button className='btn btn-outline-dark'>
-            <i className='fas fa-shopping-bag me-1'></i>
-            Bag
+          <button className='btn btn-icon' title='Bag'>
+            <FaShoppingBag />
           </button>
         </div>
-      </nav>
-
-      {showLoginModal && (
-        <LoginModal
-          onClose={() => setShowLoginModal(false)}
-          onLoginSuccess={handleLoginSuccess}
-          onOpenRegister={openRegisterFromLogin}
-        />
-      )}
-
-      {showRegisterModal && (
-        <RegisterModal
-          onClose={() => setShowRegisterModal(false)}
-          onLoginSuccess={handleLoginSuccess}
-        />
-      )}
-    </>
+      </div>
+    </nav>
   );
 };
 
